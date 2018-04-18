eeab7f7911f9f1ee56c6ae32f63359955c142d95
Ryan Jacobs <ryan@rmj.us>
2018-04-18 12:33:35 -0700

    wrap everything

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 #!/usr/bin/env node
+
+const fs = require("fs");
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+
+async function main() {
+    try {
+      //const res  = await fetch("http://menu.dining.ucla.edu/Menus");
+      //const body = await res.text();
+        const body = fs.readFileSync("./Menus").toString();
+        const $ = cheerio.load(body);
+      
+        let halls = {};
+        $(".menu-block").map(function(i, el) {
+            let name = $(el).find(".col-header").html();
+            const activity = $(el).find(".activity-level-wrapper")
+                                  .parent().text()
+                                  .split("  ")[1];
+      
+            if (activity) {
+                name = name.replace(/ at Rieber/, "");
+      
+                console.log(`${name}: ${activity}`);
+                halls[name] = parseFloat("." + activity);
+            }
+        });
+      
+        console.log(JSON.stringify(halls, null, 2));
+    } catch (e) {
+        console.log(JSON.stringify({
+            error: true,
+            message: e.message
+        }, null, 2));
+    }
+}
+
+main();
