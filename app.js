8d043d2f6520a018d961ece3a693d78c52148218
Ryan Jacobs <ryan@rmj.us>
2018-04-18 12:38:22 -0700

    wrap in try/catch

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 
-async function main() {
-    try {
-      //const res  = await fetch("http://menu.dining.ucla.edu/Menus");
-      //const body = await res.text();
-        const body = fs.readFileSync("./example.html").toString();
-        const $ = cheerio.load(body);
-      
-        let halls = {};
-        $(".menu-block").map(function(i, el) {
-            let name = $(el).find(".col-header").html();
-            const activity = $(el).find(".activity-level-wrapper")
-                                  .parent().text()
-                                  .split("  ")[1];
-      
-            if (activity) {
-                name = name.replace(/ at Rieber/, "");
-                halls[name] = parseFloat("." + activity);
-            }
-        });
-      
-        console.log(JSON.stringify(halls, null, 2));
-        return halls;
-    } catch (e) {
-        return {
-            error: true,
-            message: e.message
-        };
-    }
+function get_activity() {
+    return new Promise((resolve, reject) => {
+        try {
+          //const res  = await fetch("http://menu.dining.ucla.edu/Menus");
+          //const body = await res.text();
+            const body = fs.readFileSync("./example.html").toString();
+            const $ = cheerio.load(body);
+          
+            let halls = {};
+            $(".menu-block").map(function(i, el) {
+                let name = $(el).find(".col-header").html();
+                const activity = $(el).find(".activity-level-wrapper")
+                                      .parent().text()
+                                      .split("  ")[1];
+          
+                if (activity) {
+                    name = name.replace(/ at Rieber/, "");
+                    halls[name] = parseFloat("." + activity);
+                }
+            });
+          
+            resolve(halls);
+        } catch (e) {
+            reject({error: true, message: e.message});
+        }
+    });
 }
 
-main();
+get_activity()
+    .then(a => console.log(a))
+    .catch(e => console.log(e));
